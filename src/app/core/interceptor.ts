d7a5a69f292ccac7b6bd36b525ac865e2b469693
verma.sushant <verma.sushant@zwayam.com>
2023-09-12 10:54:30 +0530

    chatgpt-poc

--- src/app/core/interceptor.ts
+++ src/app/core/interceptor.ts
@@ -19,49 +19,15 @@ export class Interceptor implements HttpInterceptor {
   constructor(private router: Router, private alertService: AlertService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    // if (currentUser && currentUser.token) {
-    //   request = request.clone({
-    //     setHeaders: {
-    //       'Content-Type': 'application/json',
-    //       Authorization: `JWT ${currentUser.token}`
-    //     }
-    //   });
-    // }
-
-
     return next.handle(request).pipe(tap((res: any) => {
-      if (res.body && res.body.code == 401) {
-        localStorage.clear(); // clearing localstorage takes care of redirecting to login page for admin
-        window.location.reload();
-      }
-
       return;
 
     },
       (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          // if (err.status !== 401) {
-          //  return;
-          // }
-          // this.router.navigate(['login']);
-          if (err.status === 401) {
-            this.alertService.toast('error', 'Unauthorized user. Please login again!');
+        // if (err instanceof HttpErrorResponse) {
+         return false
 
-            setTimeout(() => {
-              localStorage.clear(); // clearing localstorage takes care of redirecting to login page for admin
-              window.location.reload();
-            }, 2000);
-
-          } else {
-            // let message = err.error.message.length > 0 ? err.error.message : 'Something is wrong. Please try again!';
-            console.log("something went wrong",err)
-            let message2 = err.error.message
-            this.alertService.toast('error', "something went wrong, please try again.");
-            return;
-          }
-
-        }
+        // }
       }));
 
   }
